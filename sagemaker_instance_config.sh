a2624a7db23c8c210ef26b8edde6938955f87a6f
Tian Gao <tgao@indeed.com>
2023-06-14 18:24:13 -0500

    polish the script

--- sagemaker_instance_config.sh
+++ sagemaker_instance_config.sh
@@ -4,6 +4,7 @@ if [ -d "/home/ec2-user/SageMaker/vscode" ]; then
     cd /home/ec2-user/SageMaker/vscode
 else
     # Folder does not exist
+    # download file template
     mkdir -p /home/ec2-user/SageMaker/vscode/templateGenerator
     cd /home/ec2-user/SageMaker/vscode/templateGenerator
     wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/templateGenerator/{__name__.1_notebook_sql}.ipynb
@@ -11,10 +12,9 @@ else
     wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/templateGenerator/{__name__.3_notebook_model_manual}.ipynb
     wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/templateGenerator/{__name__.4_notebook_model_sagemaker}.ipynb
 
-    
-    cd /home/ec2-user/SageMaker/vscode
-
     # download vsc extensions and other stuff
+    mkdir -p /home/ec2-user/SageMaker/vscode/extension
+    cd /home/ec2-user/SageMaker/vscode/extension
     wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/GitHub.copilot-nightly-1.88.149.vsix
     wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/GitHub.copilot-chat-0.3.2023060801.vsix
     wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/gtfish1988.gtfish01-0.1.0.vsix
@@ -22,11 +22,14 @@ else
     wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/gtfish1988.gtfish13-0.1.0.vsix
     wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/gizak.shortcuts-0.0.1.vsix
 
+    mkdir -p /home/ec2-user/SageMaker/vscode/shell
+    cd /home/ec2-user/SageMaker/vscode/shell
     wget https://github.com/tgaochn/aws_sync/raw/main/shell/bashrc
     wget https://github.com/tgaochn/aws_sync/raw/main/shell/.vimrc
     cat bashrc >> ~/.bashrc
     cp .vimrc ~/
 
+    cd /home/ec2-user/SageMaker/vscode
     wget https://github.com/tgaochn/aws_sync/raw/main/vsc%20setting/settings.json
     mkdir -p ~/SageMaker/.vscode
     cp settings.json ~/SageMaker/.vscode/settings.json
