48231708534a28dc6c6d85c2d8a7c12a283b852c
Tian Gao <tgao@indeed.com>
2023-06-14 18:13:02 -0500

    added template files

--- sagemaker_instance_config.sh
+++ sagemaker_instance_config.sh
@@ -1,29 +1,41 @@
 # download codeserver
-cd ~/SageMaker
-mkdir vscode
-cd vscode
+if [ -d "~/SageMaker/vscode" ]; then
+    # Folder exists
+    cd ~/SageMaker/vscode
+else
+    # Folder does not exist
+    mkdir -p ~/SageMaker/vscode/templateGenerator
+    cd ~/SageMaker/vscode/templateGenerator
+    wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/templateGenerator/{__name__.1_notebook_sql}
+    wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/templateGenerator/{__name__.2_python}.py
+    wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/templateGenerator/{__name__.3_notebook_model_manual}
+    wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/templateGenerator/{__name__.4_notebook_model_sagemaker}
 
-# download vsc extensions and other stuff
-wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/GitHub.copilot-nightly-1.88.149.vsix
-wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/GitHub.copilot-chat-0.3.2023060801.vsix
-wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/gtfish1988.gtfish01-0.1.0.vsix
-wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/gtfish1988.gtfish13-0.1.0.vsix
-wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/gtfish1988.gtfish12-0.1.1.vsix
-wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/gizak.shortcuts-0.0.1.vsix
+    
+    cd ~/SageMaker/vscode
 
-wget https://github.com/tgaochn/aws_sync/raw/main/shell/bashrc
-wget https://github.com/tgaochn/aws_sync/raw/main/shell/.vimrc
-cat bashrc >> ~/.bashrc
-cp .vimrc ~/
+    # download vsc extensions and other stuff
+    wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/GitHub.copilot-nightly-1.88.149.vsix
+    wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/GitHub.copilot-chat-0.3.2023060801.vsix
+    wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/gtfish1988.gtfish01-0.1.0.vsix
+    wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/gtfish1988.gtfish12-0.1.1.vsix
+    wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/gtfish1988.gtfish13-0.1.0.vsix
+    wget https://github.com/tgaochn/aws_sync/raw/main/vsc_extensions/gizak.shortcuts-0.0.1.vsix
 
-wget https://github.com/tgaochn/aws_sync/raw/main/vsc%20setting/settings.json
-mkdir -p ~/SageMaker/.vscode
-cp settings.json ~/SageMaker/.vscode/settings.json
+    wget https://github.com/tgaochn/aws_sync/raw/main/shell/bashrc
+    wget https://github.com/tgaochn/aws_sync/raw/main/shell/.vimrc
+    cat bashrc >> ~/.bashrc
+    cp .vimrc ~/
 
-# download and install codeserver
-wget https://github.com/tgaochn/aws_sync/raw/main/codeserver/notebook-instances-mod/install-codeserver.sh
-wget https://github.com/tgaochn/aws_sync/raw/main/codeserver/notebook-instances-mod/setup-codeserver.sh
-chmod +x uninstall-codeserver.sh
+    wget https://github.com/tgaochn/aws_sync/raw/main/vsc%20setting/settings.json
+    mkdir -p ~/SageMaker/.vscode
+    cp settings.json ~/SageMaker/.vscode/settings.json
+
+    # download and install codeserver
+    wget https://github.com/tgaochn/aws_sync/raw/main/codeserver/notebook-instances-mod/install-codeserver.sh
+    wget https://github.com/tgaochn/aws_sync/raw/main/codeserver/notebook-instances-mod/setup-codeserver.sh
+fi
+
 chmod +x install-codeserver.sh
 chmod +x setup-codeserver.sh
 sudo ./install-codeserver.sh
---
+++ vsc_extensions/templateGenerator/{__name__.2_python}.py
@@ -0,0 +1,17 @@
+def func():
+    pass
+
+
+# endFunc
+
+
+def main():
+    func()
+
+
+# endMain
+
+
+if __name__ == "__main__":
+    main()
+# endIf
